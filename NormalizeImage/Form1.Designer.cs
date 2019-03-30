a858245f6c3dc779020357e3b46961d577a750bd
vadwave <vadwave@yandex.ru>
2019-03-30 15:47:11 +0300

    Добавление панелей

--- NormalizeImage/Form1.Designer.cs
+++ NormalizeImage/Form1.Designer.cs
@@ -28,13 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(144, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 142);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(144, 246);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(141, 142);
+            this.panel2.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "MainForm";
             this.Text = "Нормализация изображения";
             this.ResumeLayout(false);
@@ -42,6 +60,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
